470c8a0b5634092dd5cd2526dbd32d3c684f1f37
bcrennen <bchiyeung1996@gmail.com>
2021-03-19 17:27:58 -0700

    changed background

---
+++ Scripts/GundamHealth.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GundamHealth : MonoBehaviour
+{
+    public int health;
+    public int numOfLives;
+
+    public Image[] lives;
+    public Sprite fullLives;
+    public Sprite emptyLives;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (health > numOfLives)
+        {
+            health = numOfLives;
+        }
+
+        for (int i = 0; i < lives.Length; i++)
+        {
+            if (i < health)
+            {
+                lives[i].sprite = fullLives;
+            }
+            else
+            {
+                lives[i].sprite = emptyLives;
+            }
+
+            if (i < numOfLives)
+            {
+                lives[i].enabled = true;
+            }
+            else
+            {
+                lives[i].enabled = false;
+            }
+        }
+    }
+}
---
+++ Scripts/SpaceBackgroundScroll.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceBackgroundScroll : MonoBehaviour
+{
+    // Scrolling speed
+    public float speed = 4f;
+    Vector3 StartPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // the start position is equal to current trandform value
+        // of the background image
+        // this is variabilities at the start of the game where the
+        // backgound would be
+        StartPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // want to move downwards
+        transform.Translate(Vector3.down * speed * Time.deltaTime);
+        // loop back when it hits -19.365 
+        // the outter layer of background 
+        if (transform.position.y < -19.365f)
+        {
+            transform.position = StartPosition;
+        }
+        
+    }
+}
